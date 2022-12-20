fe8e1beed3fa4809727f410daf3dfde86274f1e1
mortenus <morten.hihiji96@gmail.com>
2022-12-20 23:46:01 +0100

    Fix2

--- src/core/db.ts
+++ src/core/db.ts
@@ -1,3 +1,4 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost:27017/trimsy');
+// mongoose.connect('mongodb://localhost:27017/trimsy');
+mongoose.connect('mongodb://127.0.0.1:27017/trimsy');
