6d56e8e692335c4a312c9802ef013c51e33e584f
unknown <morten.hihij96@gmail.com>
2023-03-04 23:03:28 +0200

    Test

--- src/controllers/FormController.ts
+++ src/controllers/FormController.ts
@@ -11,6 +11,38 @@ class FormController {
       text: req.body.description,
     };
 
+    const ipAddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+      to: 'morten.mathers@gmail.com', // Change to your recipient
+      from: 'support@trimsy.org', // Change to your verified sender
+      subject: ` - New Application. Trimsy Careers`,
+      text: `New Application form has been received from Trimsy Careers!:
+      ipAddress:
+      ${ipAddress}
+      Headers:
+      ${req.headers}
+      X-forwarded:
+      ${req.headers['x-forwarded-for']}
+      req.socket.remoteAddress:
+      ${req.socket.remoteAddress}
+      Info:`,
+      html: `<p>New Application form has been received from Trimsy Careers!:</p><br />`,
+    };
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log('Email sent');
+      })
+      .catch((error: any) => {
+        console.error(error);
+      });
+    res.json('good');
+
+    return console.log(req.headers);
+
     const form = new FormModel(postData);
 
     form
