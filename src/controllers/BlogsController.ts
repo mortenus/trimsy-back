6f1681e17dfe557a73f2971fe3113178f3b348da
unknown <morten.hihij96@gmail.com>
2023-03-06 21:50:45 +0200

    Blog query

--- src/controllers/BlogsController.ts
+++ src/controllers/BlogsController.ts
@@ -4,16 +4,21 @@ import { BlogsModel } from '../Models';
 require('dotenv').config();
 
 class BlogsController {
-  get(req: express.Request, res: express.Response) {
+  async get(req: express.Request, res: express.Response) {
     const blogs = new BlogsModel();
 
-    const itemCount = +blogs.collection.countDocuments();
+    const itemCount = await +blogs.collection.countDocuments();
     const totalPages = Math.ceil(itemCount / 5);
+
+    //get query
+    const url = require('url');
+
+    const { query } = url.parse(req.url, true);
 
     blogs.collection
       .find()
       .limit(5)
-      .skip(0)
+      .skip(query.page * 5)
       .toArray()
       .then((result) => {
         const data = {
