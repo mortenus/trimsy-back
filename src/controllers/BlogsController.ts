960897c4c9034ab034ee17b028004464477cdc26
unknown <morten.hihij96@gmail.com>
2023-03-06 13:17:19 +0200

    Pagination blogs

--- src/controllers/BlogsController.ts
+++ src/controllers/BlogsController.ts
@@ -7,11 +7,21 @@ class BlogsController {
   get(req: express.Request, res: express.Response) {
     const blogs = new BlogsModel();
 
+    const itemCount = +blogs.collection.countDocuments();
+    const totalPages = Math.ceil(itemCount / 5);
+
     blogs.collection
       .find()
+      .limit(5)
+      .skip(0)
       .toArray()
       .then((result) => {
-        res.send(result);
+        const data = {
+          items: result,
+          totalPages,
+        };
+
+        res.send(data);
       })
       .catch((err) => res.status(400).json(err));
   }
